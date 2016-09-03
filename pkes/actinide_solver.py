df60e2d787181500393d753e2a228a97e4f5e09a
Bryan Herman <hermab53@gmail.com>
2016-09-03 12:10:41 -0400

    fixed up actinide decay solver

--- pkes/actinide_solver.py
+++ pkes/actinide_solver.py
@@ -11,34 +11,34 @@ NAMES = ["Am-241", "Pu-241", "Pu-240", "Pu-239", "Pu-238", "Cm-244", "Cm-242"]
 DECAY = np.array([5.078e-11, 1.531e-9, 3.3472e-12, 9.111e-13, 2.504e-10,
         1.213e-9, 4.923e-8])
 
-# Take at 20 MWd/kgU at 3 wt%
-BETA = np.array([2.868e-3, 2.147e-3, 8.586e-3, 9.721e-3, 2.778e-2,
-                 4.563e-3, 5.457e-1])
+# Take at 50 MWd/kgU at 4 wt%
+BETA = np.array([1.344e-2, 5.626e-3, 2.083e-2, 1.159e-2, 2.607e-1,
+                 3.108e-1, 4.618e0])
 
 BETA_p = BETA
 BETA_p[0] = BETA[0] - BETA[1]*(5.629/5.361e-3)*(DECAY[0]/(DECAY[0] - DECAY[1]))
 BETA_p[1] = BETA[1]*(1.0 + (5.629/5.361e-3)*(DECAY[0]/(DECAY[0] - DECAY[1])))
 
 # Initial mass of U taken from App. K in T&K
-KG_U = 89000.
+KG_U = 24000.
 
-def actinide_solve(end_time, num_steps):
+def actinide_solve(time):
 
-    time = np.zeros(num_steps + 1)
+    print("BETA IS:", BETA)
+    print("BETA_prime IS:", BETA_p)
 
-    dt = end_time / float(num_steps)
+    num_steps = len(time)
 
-    power = np.zeros((len(NAMES), num_steps + 1))
+    power = np.zeros((num_steps,))
 
-    for i in range(num_steps + 1):
+    for i in range(num_steps):
 
-        time[i] = i*dt
+        power[i] = np.sum(BETA_p*np.exp(-DECAY*time[i])) 
+        print(time[i], power[i])
 
-        power[:, i] += BETA[:]*np.exp(-DECAY[:]*time[i]) 
+    # scale specific power to actual power then convert to MW
+    power *= KG_U *1.e-6
 
-    # scale specific power to actual power
-    power *= KG_U
-
-    with open("actinide_decay.txt", "w") as fh:
-        for i in range(num_steps + 1):
-            fh.write("{0} {1}\n".format(time[i], " ".join(map(str, power[:, i]))))
+    with open("actinide_decay.dat", "w") as fh:
+        for i in range(num_steps):
+            fh.write("{0} {1}\n".format(time[i], power[i])) 
--- results/actinide.py
+++ results/actinide.py
@@ -1,3 +1,15 @@
 import pkes
 
-pkes.actinide_solve(4000.0, 4000)
+import numpy as np
+
+with open('decay_power_new.dat') as handle:
+    lines = handle.read().splitlines()
+
+decay_time = np.empty((len(lines),), dtype=float)
+power_interp = np.empty((len(lines),), dtype=float)
+
+for i, aline in enumerate(lines):
+    sline = aline.split()
+    decay_time[i] = float(sline[0])
+
+pkes.actinide_solve(decay_time)
