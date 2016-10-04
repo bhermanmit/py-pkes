6fd36e85958b0914bb3bdc8567cddf21a07da154
Bryan Herman <hermab53@gmail.com>
2016-10-03 21:15:47 -0400

    updated for anaytic model

--- results/process_data.py
+++ results/process_data.py
@@ -1,5 +1,24 @@
 import numpy as np
 
+POWER = 3000.
+
+def analytic(time):
+
+    if time <= 400.0:
+        res = -6.14575e-3*np.log(time) + 0.060157
+    elif 400 < time <= 4.e5:
+        res = 1.40680e-1*time**-0.286
+    elif 4.e5 < time <= 4.e6:
+        res = 8.70300e-1*time**-0.4255
+    elif 4.e6 < time <= 4.e7:
+        res = 1.28420e1*time**-0.6014
+    elif 4.e7 < time <= 4.e8:
+        res = 4.03830e4*time**-1.0675
+    else:
+        res = 3.91130e-5*np.exp(-7.3541e-10*time)
+
+    return res
+
 with open('power.dat') as handle:
     lines = handle.read().splitlines()
 
@@ -24,4 +43,4 @@ for i, aline in enumerate(lines):
 
 with open('power_interp.dat', 'w') as handle:
    for time, power in zip(decay_time, power_interp):
-       handle.write("{} {}\n".format(time, power))
+       handle.write("{} {} {}\n".format(time, power, POWER*analytic(time)))
